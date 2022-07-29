657258aa82fe648a03aa15862d3a53f084dc1a96
reezan_s <reezansmith@gmail.com>
2022-07-29 10:04:57 +0900

    Created function to fetch repository data

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const overviewDiv = document.querySelector(".overview");
-
 const username = "ReeAya";
+const repoList = document.querySelector(".repo-list");
 
 const gitUserInfo = async function() {
     const userInfo = await fetch("`https://api.github.com/users/${username}`");
@@ -25,4 +25,22 @@ const displayUserInfo = function(data) {
     </div>
     `;
     overviewDiv.append(div);
+    gitRepos();
+};
+
+const gitRepos = async function() {
+    const fetchRepos = await fetch(
+        `https://api.github.com/users/${username}/repos?sort=updated&per_page=100`
+    );
+    const repodata = await fetchRepos.json();
+    displayRepos(repodata);
+};
+
+const displayRepos = function() {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
